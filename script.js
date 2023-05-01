1d11cf1bec63433a16ce3b77bcfa59549388d705
alees <nprolol8@gmail.com>
2023-05-01 19:18:58 +0300

    end-project

---
+++ script.js
@@ -0,0 +1,47 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let saveButton = document.querySelector('.popup__button');
+let popupName = document.querySelector('.popup__item_name');
+let popupProfession = document.querySelector('.popup__item_profession');
+let popupNameValue = popupName.querySelector('input');
+let popupProfessionValue = popupProfession.querySelector('input');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let closeButton = document.querySelector('.popup__close-button');
+
+editButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+  popupNameValue.setAttribute('value', profileName.textContent);
+  popupProfessionValue.setAttribute('value', profileProfession.textContent);
+}); 
+
+saveButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupNameValue.value;
+  profileProfession.textContent = popupProfessionValue.value;
+  popup.classList.remove('popup_opened');
+}); 
+
+closeButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
